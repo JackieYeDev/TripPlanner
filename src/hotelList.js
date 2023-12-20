4a9f9c68e3c4875aca8c5348568f284a803ac166
Jackie <jye0325@gmail.com>
2023-12-20 09:18:56 -0500

    Add hotel result display to modal.

---
+++ src/hotelList.js
@@ -0,0 +1,51 @@
+class HotelList {
+    constructor(list) {
+        this.list = list;
+    }
+
+    render() {
+        if(this.list.length === 0) return "Unable to find hotel(s).";
+
+        // Create HTML Element(s)
+        const listGroup = document.createElement('ul');
+        this.list.forEach((item, index) => {
+            const listItem = document.createElement('li');
+            const itemTitle = document.createElement('h5');
+            const itemDiv = document.createElement('div');
+            const itemDesc = document.createElement('span');
+            const addButton = document.createElement('button');
+
+            // Assign class name(s) to HTML Element(s)
+            listItem.className = 'list-group-item';
+            addButton.className = 'btn btn-primary float-end';
+
+            // Assign id to HTML Element(s)
+            listItem.id = `${index}`;
+
+            // Assign properties to HTML ELement(s)
+            itemTitle.textContent = item.name;
+            itemDesc.textContent = item.location.formatted_address;
+            addButton.textContent = "Add";
+
+            // Add event listener(s) to HTML Element(s)
+            addButton.addEventListener('click', ()=>{
+
+            });
+
+            // Append Elements to Respective Containers
+            itemDiv.append(itemDesc);
+            listItem.append(itemTitle, itemDiv, addButton);
+            listGroup.append(listItem);
+        })
+
+        // Assign class name(s) to HTML Element(s)
+        listGroup.className = 'list-group';
+
+        // Assign style(s) to HTML ELement(s)
+        listGroup.style = 'list-style-type: none;';
+
+        return listGroup;
+    }
+}
+
+export default HotelList;
--- src/modal.js
+++ src/modal.js
@@ -1,18 +1,24 @@
+import HotelList from "./hotelList.js";
+
 class Modal {
-    constructor() {
+    constructor(results) {
         this.hotelModal = document.querySelector('div#hotel-modal');
+        this.results = results;
     }
     render(title = 'Modal Title') {
+        // Create HTML Element(s)
         const modalDialog = document.createElement('div');
         const modalContent = document.createElement('div');
         const modalHeader = document.createElement('div');
         const modalBody = document.createElement('div');
         const modalFooter = document.createElement('div');
         const modalTitle = document.createElement('h4');
+        const modalBodyText = document.createElement('p');
         const closeButton = document.createElement('button');
         const closeButton2 = document.createElement('button');
-        const addButton = document.createElement('button');
+        const hotelList = new HotelList(this.results).render();
 
+        // Assign class name(s) to HTML Element(s)
         modalDialog.className = 'modal-dialog';
         modalContent.className = 'modal-content';
         modalHeader.className = 'modal-header';
@@ -21,37 +27,47 @@ class Modal {
         modalTitle.className = 'modal-title';
         closeButton.className = 'btn-close';
         closeButton2.className = 'btn btn-light';
-        addButton.className = 'btn btn-primary';
 
-
+        // Assign properties to HTML ELement(s)
         modalDialog.role = 'document';
         modalTitle.textContent = title;
+        modalBodyText.textContent = "Please select a hotel to add to the itinerary!";
         closeButton.type = 'button';
         closeButton.ariaLabel = 'Close';
         closeButton.setAttribute('data-bs-dismiss', 'modal');
         closeButton2.type = 'button';
         closeButton2.innerText = 'Close';
         closeButton2.setAttribute('data-bs-dismiss', 'modal');
-        addButton.type = 'button';
-        addButton.innerText = 'Add Hotel';
 
+        // Add event listener(s) to HTML Element(s)
         closeButton.addEventListener('click', (e) => {
             this.hotelModal.className = this.hotelModal.className.replace('show', 'hide');
             this.hotelModal.style = 'display: none;';
+            this.destroy();
+            delete window[this];
         });
 
         closeButton2.addEventListener('click', (e) => {
             this.hotelModal.className = this.hotelModal.className.replace('show', 'hide');
             this.hotelModal.style = 'display: none;';
+            this.destroy();
+            delete window[this];
         });
 
+        // Append Elements to Respective Containers
         modalHeader.append(modalTitle, closeButton);
-        modalFooter.append(closeButton2, addButton);
+        modalFooter.append(closeButton2);
+        modalBody.append(modalBodyText, hotelList);
         modalContent.append(modalHeader, modalBody, modalFooter);
         modalDialog.append(modalContent);
         this.hotelModal.append(modalDialog);
         this.hotelModal.className = this.hotelModal.className.replace('hide', 'show');
         this.hotelModal.style = 'display: block;';
+
+    }
+
+    destroy() {
+        this.hotelModal.innerHTML = "";
     }
 
 }
